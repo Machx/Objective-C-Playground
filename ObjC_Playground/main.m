d617fe3d3889a6b8e3e45d9f4039a13b2b472a99
Colin Wheeler <cocoasamurai@gmail.com>
2012-10-26 11:07:12 -0500

    Initial Commit

---
+++ ObjC_Playground/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 10/26/12.
+//  Copyright (c) 2012 Colin Wheeler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
