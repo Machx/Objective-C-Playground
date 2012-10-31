82e8316c7ea5c0e81eb9ac03de105c1b36171de7
Colin Wheeler <cocoasamurai@gmail.com>
2012-10-31 15:11:00 -0500

    starting to setup block notification center tests

---
+++ ObjC_PlaygroundTests/CWBlockNotificationCenterTests.h
@@ -0,0 +1,13 @@
+//
+//  CWBlockNotificationCenterTests.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 10/31/12.
+//  Copyright (c) 2012 Colin Wheeler. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWBlockNotificationCenterTests : SenTestCase
+
+@end
---
+++ ObjC_PlaygroundTests/CWBlockNotificationCenterTests.m
@@ -0,0 +1,34 @@
+//
+//  CWBlockNotificationCenterTests.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 10/31/12.
+//  Copyright (c) 2012 Colin Wheeler. All rights reserved.
+//
+
+#import "CWBlockNotificationCenterTests.h"
+#import "CWBlockNotificationCenter.h"
+
+@interface CWBlockNotificationCenterTests ()
+@property(assign) BOOL triggered;
+@end
+
+@implementation CWBlockNotificationCenterTests
+
+-(void)setUp
+{
+	[[CWBlockNotificationCenter defaultCenter] addBlock:^{
+		NSLog(@"Got notified");
+		self.triggered = NO;
+	} forNotification:@"name"
+              onQueue:dispatch_get_main_queue()];
+}
+
+-(void)testBasic
+{
+	[[CWBlockNotificationCenter defaultCenter] postNotificationWithName:@"name"];
+	
+	STAssertTrue(self.triggered, nil);
+}
+
+@end
