23e447b6316a50d86dd227a3bc0796f434c682ed
Colin Wheeler <cocoasamurai@gmail.com>
2013-01-07 09:37:02 -0600

    document CWPriorityQueue

--- ObjC_Playground/CWPriorityQueue.h
+++ ObjC_Playground/CWPriorityQueue.h
@@ -14,11 +14,34 @@
 
 @interface CWPriorityQueue : NSObject
 
+/**
+ Adds the item to the queue and sorts all items by their priority
+ 
+ The item is added to the Queue and then sorted by the priority it was given in
+ this API. The lower an items number is the higher its priority is in the queue.
+ 
+ @param item to be added to the queue
+ @param priority this number is used to sort the item in the queue
+ */
 -(void)addItem:(id)item //lower the # higher the priority
   withPriority:(NSUInteger)priority;
 
+/**
+ Removes an item with the highest priority off the queue & returns it
+ 
+ This method grabs a reference to the item with the highest priority on the
+ queue (priority 0 or as close to it as there is), removes it from the queue
+ and then returns it to you.
+ 
+ @return the item with the highest priority (lowest #) or nil if queue is empty
+ */
 -(id)dequeue;
 
+/**
+ Returns all objects that have a priority in the queue that matches the argument
+ 
+ @return an NSSet of all objects matching the given priority, or an empty set
+ */
 -(NSSet *)allObjectsOfPriority:(NSUInteger)priority;
 
 @end
