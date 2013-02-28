f8caff88e646f3a30d6cd617ee40916da30113c8
Colin Wheeler <cocoasamurai@gmail.com>
2013-02-28 09:19:11 -0600

    remove old unit tests

--- ObjC_PlaygroundTests/ObjC_PlaygroundTests.h
+++
@@ -1,9 +0,0 @@
-//
-//  ObjC_PlaygroundTests.h
-//  ObjC_PlaygroundTests
-//
-//  Created by Colin Wheeler on 10/26/12.
-//  Copyright (c) 2012 Colin Wheeler. All rights reserved.
-//
-
-#import "ObjC_Playground-Prefix.pch"
