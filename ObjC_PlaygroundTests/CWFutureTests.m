5223d1a14f13ed0b66170145d3267b0746d63417
Colin Wheeler <cocoasamurai@gmail.com>
2013-03-01 10:22:41 -0600

    update unit tests
    - added before all and after all for testing if the future was resolved
    - added test to test future class
    - added test to test future hash

--- ObjC_PlaygroundTests/CWFutureTests.m
+++ ObjC_PlaygroundTests/CWFutureTests.m
@@ -10,14 +10,34 @@
 #import "CWFuture.h"
 #import "CWBlockNotificationCenter.h"
 
+CWFuture *future = nil;
+
 SpecBegin(CWFuture)
 
-it(@"should capture an expression and return its result", ^{
-	CWFuture *future = [CWFuture futureWithBlock:^id{
-		return @4;
-	}];
+beforeAll(^{
+	future = [CWFuture futureWithBlock:^id{ return @(3 + 1); }];
 	
+	it(@"should return NO when the future value hasn't been resolved", ^{
+		expect([future futureResolved]).to.beFalsy();
+	});
+});
+
+it(@"should capture an expression and return its result", ^{
 	expect([future resolveFuture]).to.equal(@4);
+});
+
+it(@"should return the class of the resolved future value", ^{
+	expect([future class] != [CWFuture class]).to.beTruthy();
+});
+
+it(@"should return the hash of the resolved future", ^{
+	expect([future hash] == [[future resolveFuture] hash]).to.beTruthy();
+});
+
+afterAll(^{
+	it(@"should return YES when the future value has been resolved", ^{
+		expect([future futureResolved]).to.beTruthy();
+	});
 });
 
 SpecEnd
