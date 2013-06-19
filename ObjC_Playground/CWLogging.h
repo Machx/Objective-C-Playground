41d319772a912b440b8013f90531235bda7e940a
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-19 10:58:02 -0500

    add initial documentation on log levels

--- ObjC_Playground/CWLogging.h
+++ ObjC_Playground/CWLogging.h
@@ -33,6 +33,42 @@
 #import <Foundation/Foundation.h>
 #import <asl.h>
 
+/**
+ CW_LOG_LEVELS exactly match ASL_LOG levels as defined in asl.h
+ #define ASL_LEVEL_EMERG   0
+ #define ASL_LEVEL_ALERT   1
+ #define ASL_LEVEL_CRIT    2
+ #define ASL_LEVEL_ERR     3
+ #define ASL_LEVEL_WARNING 4
+ #define ASL_LEVEL_NOTICE  5
+ #define ASL_LEVEL_INFO    6
+ #define ASL_LEVEL_DEBUG   7
+ 
+ Emergency (level 0)
+ The highest priority, usually reserved for catastrophic failures and reboot notices.
+ 
+ Alert (level 1)
+ A serious failure in a key system.
+ 
+ Critical (level 2)
+ A failure in a key system.
+ 
+ Error (level 3)
+ Something has failed.
+ 
+ Warning (level 4)
+ Something is amiss and might fail if not corrected.
+ 
+ Notice (level 5)
+ Things of moderate interest to the user or administrator.
+ 
+ Info (level 6)
+ The lowest priority that you would normally log, and purely informational in nature.
+ 
+ Debug (level 7)
+ The lowest priority, and normally not logged except for messages from the kernel.
+ */
+
 #ifndef CW_LOG_LEVEL
 	#ifdef DEBUG
 		#define CW_LOG_LEVEL ASL_LEVEL_DEBUG
