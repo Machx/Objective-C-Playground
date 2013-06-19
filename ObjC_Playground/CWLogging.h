3e39d456de5996f0a09badac91ad3d35e7173ee4
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-19 10:43:05 -0500

    add CWLogging

---
+++ ObjC_Playground/CWLogging.h
@@ -0,0 +1,13 @@
+//
+//  CWLogging.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 6/19/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWLogging : NSObject
+
+@end
---
+++ ObjC_Playground/CWLogging.m
@@ -0,0 +1,13 @@
+//
+//  CWLogging.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 6/19/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWLogging.h"
+
+@implementation CWLogging
+
+@end
