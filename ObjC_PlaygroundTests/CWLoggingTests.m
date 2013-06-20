3cea3964995138c886b17c1911f9208c21e2fc53
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-20 09:13:47 -0500

    basic way to run CWLog2

--- ObjC_PlaygroundTests/CWLoggingTests.m
+++ ObjC_PlaygroundTests/CWLoggingTests.m
@@ -6,8 +6,27 @@
 //  Copyright (c) 2013 Colin Wheeler. All rights reserved.
 //
 
+//#define CW_LOG_LEVEL 3
+
 #import "CWLoggingTests.h"
+#import "CWLogging.h"
 
-@implementation CWLoggingTests
+SpecBegin(CWLogging)
 
-@end
+it(@"should log", ^{
+	/**
+	 This is here as a means to verify that the function 
+	 is working correctly. It is not a true unit test.
+	 */
+//	CWLog2(7, @"Log Debug Message");
+//	CWLog2(6, @"Log Info Message");
+//	CWLog2(5, @"Log Notice Message");
+//	CWLog2(4, @"Log Warning Message");
+//	CWLog2(3, @"Log Error Message");
+//	CWLog2(2, @"Log Critical Message");
+//	CWLog2(1, @"Log Alert Message");
+//	CWLog2(0, @"Log Emergency Message");
+//	CWLog2(7, @"Log Var arg Message %@",@"Hello World!");
+});
+
+SpecEnd
