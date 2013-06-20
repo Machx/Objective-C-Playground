bd588d57a6572dc74a634a4abdc1ad1c242a9f3d
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-20 09:10:53 -0500

    add CWLoggingTests

---
+++ ObjC_PlaygroundTests/CWLoggingTests.h
@@ -0,0 +1,13 @@
+//
+//  CWLoggingTests.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 6/19/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWLoggingTests : SenTestCase
+
+@end
---
+++ ObjC_PlaygroundTests/CWLoggingTests.m
@@ -0,0 +1,13 @@
+//
+//  CWLoggingTests.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 6/19/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWLoggingTests.h"
+
+@implementation CWLoggingTests
+
+@end
