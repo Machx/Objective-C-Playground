1e1fb25d5208b4cc642aee84e5355fa54275b7a1
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-21 09:08:05 -0500

    tests now use the CWLOG_ defines

--- ObjC_PlaygroundTests/CWLoggingTests.m
+++ ObjC_PlaygroundTests/CWLoggingTests.m
@@ -18,15 +18,15 @@ it(@"should log", ^{
 	 This is here as a means to verify that the function 
 	 is working correctly. It is not a true unit test.
 	 */
-//	CWLog2(7, @"Log Debug Message");
-//	CWLog2(6, @"Log Info Message");
-//	CWLog2(5, @"Log Notice Message");
-//	CWLog2(4, @"Log Warning Message");
-//	CWLog2(3, @"Log Error Message");
-//	CWLog2(2, @"Log Critical Message");
-//	CWLog2(1, @"Log Alert Message");
-//	CWLog2(0, @"Log Emergency Message");
-//	CWLog2(7, @"Log Var arg Message %@",@"Hello World!");
+//	CWLog2(CWLOG_DEBUG, @"Log Debug Message");
+//	CWLog2(CWLOG_INFO, @"Log Info Message");
+//	CWLog2(CWLOG_NOTICE, @"Log Notice Message");
+//	CWLog2(CWLOG_WARNING, @"Log Warning Message");
+//	CWLog2(CWLOG_ERR, @"Log Error Message");
+//	CWLog2(CWLOG_CRIT, @"Log Critical Message");
+//	CWLog2(CWLOG_ALERT, @"Log Alert Message");
+//	CWLog2(CWLOG_EMERG, @"Log Emergency Message");
+//	CWLog2(CWLOG_DEBUG, @"Log Var arg Message %@",@"Hello World!");
 });
 
 SpecEnd
