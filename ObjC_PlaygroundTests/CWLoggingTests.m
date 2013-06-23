8bffaa909f0c7c4880bf0a3860ae68e599aea86f
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-23 11:05:39 -0500

    convert levels to ASL levels

--- ObjC_PlaygroundTests/CWLoggingTests.m
+++ ObjC_PlaygroundTests/CWLoggingTests.m
@@ -18,15 +18,15 @@ it(@"should log", ^{
 	 This is here as a means to verify that the function 
 	 is working correctly. It is not a true unit test.
 	 */
-//	CWLog2(CWLOG_DEBUG, @"Log Debug Message");
-//	CWLog2(CWLOG_INFO, @"Log Info Message");
-//	CWLog2(CWLOG_NOTICE, @"Log Notice Message");
-//	CWLog2(CWLOG_WARNING, @"Log Warning Message");
-//	CWLog2(CWLOG_ERR, @"Log Error Message");
-//	CWLog2(CWLOG_CRIT, @"Log Critical Message");
-//	CWLog2(CWLOG_ALERT, @"Log Alert Message");
-//	CWLog2(CWLOG_EMERG, @"Log Emergency Message");
-//	CWLog2(CWLOG_DEBUG, @"Log Var arg Message %@",@"Hello World!");
+//	CWLog2(ASL_LEVEL_DEBUG, @"Log Debug Message");
+//	CWLog2(ASL_LEVEL_INFO, @"Log Info Message");
+//	CWLog2(ASL_LEVEL_NOTICE, @"Log Notice Message");
+//	CWLog2(ASL_LEVEL_WARNING, @"Log Warning Message");
+//	CWLog2(ASL_LEVEL_ERR, @"Log Error Message");
+//	CWLog2(ASL_LEVEL_CRIT, @"Log Critical Message");
+//	CWLog2(ASL_LEVEL_ALERT, @"Log Alert Message");
+//	CWLog2(ASL_LEVEL_EMERG, @"Log Emergency Message");
+//	CWLog2(ASL_LEVEL_DEBUG, @"Log Var arg Message %@",@"Hello World!");
 });
 
 SpecEnd
