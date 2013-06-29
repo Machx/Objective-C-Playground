4bb8a1e058862579541bcf3b614a042914c1579a
Colin Wheeler <cocoasamurai@gmail.com>
2013-06-29 12:27:02 -0500

    remove CWLogging

--- ObjC_PlaygroundTests/CWLoggingTests.h
+++
@@ -1,9 +0,0 @@
-//
-//  CWLoggingTests.h
-//  ObjC_Playground
-//
-//  Created by Colin Wheeler on 6/19/13.
-//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
-//
-
-#import <SenTestingKit/SenTestingKit.h>
