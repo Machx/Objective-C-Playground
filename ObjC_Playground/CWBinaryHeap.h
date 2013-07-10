08585e29ea35a530e33b6cbe2a3ccc863507d1ba
Colin Wheeler <cocoasamurai@gmail.com>
2013-07-10 11:41:30 -0500

    add CWBinaryHeap files

---
+++ ObjC_Playground/CWBinaryHeap.h
@@ -0,0 +1,13 @@
+//
+//  CWBinaryHeap.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 7/10/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWBinaryHeap : NSObject
+
+@end
---
+++ ObjC_Playground/CWBinaryHeap.m
@@ -0,0 +1,13 @@
+//
+//  CWBinaryHeap.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 7/10/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWBinaryHeap.h"
+
+@implementation CWBinaryHeap
+
+@end
