523d65d7033192b2aeceefe8872ced3877a9d644
Colin Wheeler <cocoasamurai@gmail.com>
2013-07-10 11:42:30 -0500

    initial work on the CWBinaryHeap code

--- ObjC_Playground/CWBinaryHeap.m
+++ ObjC_Playground/CWBinaryHeap.m
@@ -1,13 +1,84 @@
-//
+/*
 //  CWBinaryHeap.m
 //  ObjC_Playground
 //
 //  Created by Colin Wheeler on 7/10/13.
 //  Copyright (c) 2013 Colin Wheeler. All rights reserved.
 //
+ 
+ Copyright (c) 2013, Colin Wheeler
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "CWBinaryHeap.h"
 
+#pragma callbacks
+
+static const void* CWBinaryHeapRetain(CFAllocatorRef allocator, const void *ptr) {
+	return CFBridgingRetain((__bridge id)ptr);
+}
+
+static void CWBinaryHeapRelease(CFAllocatorRef allocator, const void *ptr) {
+	CFBridgingRelease(ptr);
+}
+
+static CFStringRef CWBinaryHeapCopyDescription(const void *ptr) {
+	NSObject *event = (__bridge NSObject *)ptr;
+	CFStringRef description = (__bridge CFStringRef)[event description];
+	return description;
+}
+
+static CFComparisonResult CWBinaryHeapCompare(const void *ptr1, const void *ptr2, void *context) {
+	NSObject *obj1 = (__bridge NSObject *)ptr1;
+	NSObject *obj2 = (__bridge NSObject *)ptr2;
+	
+	id<CWBinaryHeapComparitor> comparitor = (__bridge id<CWBinaryHeapComparitor>)ptr1;
+	return [comparitor compare:obj1 to:obj2];
+}
+
+#pragma class extension
+
+@interface CWBinaryHeap ()
+@property(assign) CFBinaryHeapRef heap;
+@end
+
 @implementation CWBinaryHeap
+
+- (id)init
+{
+    self = [super init];
+    if (!self) return self;
+	
+	CFBinaryHeapCallBacks callBacks;
+	callBacks.retain = CWBinaryHeapRetain;
+	callBacks.release = CWBinaryHeapRelease;
+	callBacks.copyDescription = CWBinaryHeapCopyDescription;
+	callBacks.compare = CWBinaryHeapCompare;
+	
+	_heap = CFBinaryHeapCreate(kCFAllocatorDefault, 0, &callBacks, NULL);
+	
+    return self;
+}
 
 @end
