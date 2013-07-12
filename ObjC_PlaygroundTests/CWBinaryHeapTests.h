b642f6a8902c02fd1647e54a414271dcade1ea06
Colin Wheeler <cocoasamurai@gmail.com>
2013-07-12 10:07:59 -0500

    add CWBinaryHeapTests

---
+++ ObjC_PlaygroundTests/CWBinaryHeapTests.h
@@ -0,0 +1,13 @@
+//
+//  CWBinaryHeapTests.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 7/12/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWBinaryHeapTests : SenTestCase
+
+@end
---
+++ ObjC_PlaygroundTests/CWBinaryHeapTests.m
@@ -0,0 +1,13 @@
+//
+//  CWBinaryHeapTests.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 7/12/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWBinaryHeapTests.h"
+
+@implementation CWBinaryHeapTests
+
+@end
