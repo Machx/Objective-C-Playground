a154d4aaaa96d9866ecf0ee5eb9083724027a7fd
Colin Wheeler <cocoasamurai@gmail.com>
2013-07-25 08:57:40 -0500

    add CWBlockTimer2

---
+++ ObjC_Playground/CWBlockTimer2.h
@@ -0,0 +1,13 @@
+//
+//  CWBlockTimer.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 7/25/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWBlockTimer2 : NSObject
+
+@end
---
+++ ObjC_Playground/CWBlockTimer2.m
@@ -0,0 +1,13 @@
+//
+//  CWBlockTimer.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 7/25/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWBlockTimer2.h"
+
+@implementation CWBlockTimer2
+
+@end
