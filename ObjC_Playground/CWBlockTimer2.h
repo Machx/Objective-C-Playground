f805b265618750d9af7462d6d629161ef5cc8da8
Colin Wheeler <cocoasamurai@gmail.com>
2013-07-26 09:31:23 -0500

    document the init and invalidate methods

--- ObjC_Playground/CWBlockTimer2.h
+++ ObjC_Playground/CWBlockTimer2.h
@@ -10,11 +10,21 @@
 
 @interface CWBlockTimer2 : NSObject
 
-//designated initializer
+/**
+ CWBlockTimer2 Designated Initializer
+ 
+ @param interval seconds between timer firing
+ @param queue block should be executed on. Must not be null
+ @param block to be executed by the timer
+ @return a new CWBlockTimer2 configured instance
+ */
 - (id)initWithTimeInterval:(NSTimeInterval)interval
 				   onQueue:(dispatch_queue_t)queue
 				 withBlock:(dispatch_block_t)block;
 
+/**
+ Invalidates the timer so it stops firing
+ */
 -(void)invalidate;
 
 @end
