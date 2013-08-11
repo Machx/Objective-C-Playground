e15a044370927dc92ec6a3983416da140be7c759
Colin Wheeler <cocoasamurai@gmail.com>
2013-08-11 10:06:29 -0500

    add CWTree2

---
+++ ObjC_Playground/CWTree2.h
@@ -0,0 +1,13 @@
+//
+//  CWTree2.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 8/11/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWTree2 : NSObject
+
+@end
---
+++ ObjC_Playground/CWTree2.m
@@ -0,0 +1,13 @@
+//
+//  CWTree2.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 8/11/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWTree2.h"
+
+@implementation CWTree2
+
+@end
