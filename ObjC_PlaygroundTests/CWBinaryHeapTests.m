dc33eafaf77f1b7d7b927fa67aff26b1dc2bc86e
Colin Wheeler <cocoasamurai@gmail.com>
2013-09-20 09:20:31 -0500

    wrap the inidvidual api specific tests in a describe block for organization

--- ObjC_PlaygroundTests/CWBinaryHeapTests.m
+++ ObjC_PlaygroundTests/CWBinaryHeapTests.m
@@ -11,48 +11,54 @@
 
 SpecBegin(CWBinaryHeap)
 
-it(@"should have correct count", ^{
-	CWBinaryHeap *heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
-		return NSOrderedSame;
-	}];
-	
-	expect(heap.count == 0).to.beTruthy();
-	
-	[heap addObject:@"hello"];
-	
-	expect(heap.count == 1).to.beTruthy();
+describe(@"-count", ^{
+	it(@"should have correct count", ^{
+		CWBinaryHeap *heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
+			return NSOrderedSame;
+		}];
+		
+		expect(heap.count == 0).to.beTruthy();
+		
+		[heap addObject:@"hello"];
+		
+		expect(heap.count == 1).to.beTruthy();
+	});
 });
 
-it(@"should remove the minimum Value & return it", ^{
-	CWBinaryHeap *heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
-		return [obj1 compare:obj2];
-	}];
-	
-	[heap addObject:@1];
-	[heap addObject:@2];
-	[heap addObject:@3];
-	[heap addObject:@4];
-	[heap addObject:@5];
-	
-	expect(heap.count == 5).to.beTruthy();
-	
-	id minimum = [heap removeMinimumValue];
-	
-	expect(minimum).notTo.beNil();
-	expect(minimum).to.equal(@1);
-	expect(heap.count == 4).to.beTruthy();
+describe(@"-removeMinimumValue:", ^{
+	it(@"should remove the minimum Value & return it", ^{
+		CWBinaryHeap *heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
+			return [obj1 compare:obj2];
+		}];
+		
+		[heap addObject:@1];
+		[heap addObject:@2];
+		[heap addObject:@3];
+		[heap addObject:@4];
+		[heap addObject:@5];
+		
+		expect(heap.count == 5).to.beTruthy();
+		
+		id minimum = [heap removeMinimumValue];
+		
+		expect(minimum).notTo.beNil();
+		expect(minimum).to.equal(@1);
+		expect(heap.count == 4).to.beTruthy();
+	});
 });
 
-it(@"should correctly report back if it contains an object", ^{
-	CWBinaryHeap *heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
-		return [obj1 compare:obj2];
-	}];
-	
-	expect([heap containsObject:@4]).to.beFalsy();
-	
-	[heap addObject:@4];
-	
-	expect([heap containsObject:@4]).to.beTruthy();
+describe(@"-containsObject:(id)object", ^{
+	it(@"should correctly report back if it contains an object", ^{
+		CWBinaryHeap *heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
+			return [obj1 compare:obj2];
+		}];
+		
+		expect([heap containsObject:@4]).to.beFalsy();
+		
+		[heap addObject:@4];
+		
+		expect([heap containsObject:@4]).to.beTruthy();
+	});
 });
 
 SpecEnd
