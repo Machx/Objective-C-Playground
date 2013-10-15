1c5cbf6e27bde16cae8de074385b59af18eef8d9
Colin Wheeler <cocoasamurai@gmail.com>
2013-10-15 09:10:34 -0500

    unit tests for CWTree2

---
+++ ObjC_PlaygroundTests/CWTree2Tests.h
@@ -0,0 +1,13 @@
+//
+//  CWTree2Tests.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 10/15/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CWTree2Tests : SenTestCase
+
+@end
---
+++ ObjC_PlaygroundTests/CWTree2Tests.m
@@ -0,0 +1,13 @@
+//
+//  CWTree2Tests.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 10/15/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWTree2Tests.h"
+
+@implementation CWTree2Tests
+
+@end
