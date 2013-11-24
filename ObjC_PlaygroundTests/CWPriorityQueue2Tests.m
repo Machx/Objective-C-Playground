a4ba5076c464ff8f5748a830927c16f307ad8d67
Colin Wheeler <cocoasamurai@gmail.com>
2013-11-24 11:30:41 -0600

    update unit tests with new api

--- ObjC_PlaygroundTests/CWPriorityQueue2Tests.m
+++ ObjC_PlaygroundTests/CWPriorityQueue2Tests.m
@@ -14,12 +14,12 @@ SpecBegin(CWPriorityQueue2)
 it(@"should dequeue objects in order", ^{
     CWPriorityQueue2 *queue = [[CWPriorityQueue2 alloc] init];
     
-    [queue enqueue:@45];
-    [queue enqueue:@54];
-    [queue enqueue:@22];
-    [queue enqueue:@31];
-    [queue enqueue:@90];
-    [queue enqueue:@10];
+    [queue enqueue:@45 withPriority:@45];
+    [queue enqueue:@54 withPriority:@54];
+    [queue enqueue:@22 withPriority:@22];
+    [queue enqueue:@31 withPriority:@31];
+    [queue enqueue:@90 withPriority:@90];
+    [queue enqueue:@10 withPriority:@10];
     
     expect([queue dequeue]).to.equal(@10);
     expect([queue dequeue]).to.equal(@22);
