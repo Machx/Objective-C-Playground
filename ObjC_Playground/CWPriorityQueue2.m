703e0ddc1cadd8fb4fd23aa3feb435b78c83c8ca
Colin Wheeler <cocoasamurai@gmail.com>
2013-11-25 09:11:32 -0600

    test of CWPriorityQueue2 using associated objects

--- ObjC_Playground/CWPriorityQueue2.m
+++ ObjC_Playground/CWPriorityQueue2.m
@@ -8,15 +8,18 @@
 
 #import "CWPriorityQueue2.h"
 #import "CWBinaryHeap.h"
+#import <objc/runtime.h>
 
-@interface CWPriorityQueue2Node : NSObject
-@property(strong) id containedValue;
-@property(strong) NSNumber *priority;
-@end
+//@interface CWPriorityQueue2Node : NSObject
+//@property(strong) id containedValue;
+//@property(strong) NSNumber *priority;
+//@end
 
-@implementation CWPriorityQueue2Node
+//@implementation CWPriorityQueue2Node
+//
+//@end
 
-@end
+void *kCWPriorityQueue2ObjKey = &kCWPriorityQueue2ObjKey;
 
 @interface CWPriorityQueue2 ()
 @property(strong) CWBinaryHeap *heap;
@@ -29,24 +32,31 @@
     if(!self) return self;
     
     _heap = [[CWBinaryHeap alloc] initWithSortBlock:^NSComparisonResult(id obj1, id obj2) {
-        CWPriorityQueue2Node *node1 = (CWPriorityQueue2Node *)obj1;
-        CWPriorityQueue2Node *node2 = (CWPriorityQueue2Node *)obj2;
-        return [node1.priority compare:node2.priority];
+        //CWPriorityQueue2Node *node1 = (CWPriorityQueue2Node *)obj1;
+        //CWPriorityQueue2Node *node2 = (CWPriorityQueue2Node *)obj2;
+        NSNumber *obj1Priority = objc_getAssociatedObject(obj1, kCWPriorityQueue2ObjKey);
+        NSNumber *obj2Priority = objc_getAssociatedObject(obj2, kCWPriorityQueue2ObjKey);
+        return [obj1Priority compare:obj2Priority];
     }];
     
     return self;
 }
 
 -(void)enqueue:(id)obj withPriority:(NSNumber *)priority {
-    CWPriorityQueue2Node *node = [CWPriorityQueue2Node new];
-    node.containedValue = obj;
-    node.priority = priority;
-    [self.heap addObject:node];
+    //CWPriorityQueue2Node *node = [CWPriorityQueue2Node new];
+    //node.containedValue = obj;
+    //node.priority = priority;
+    //[self.heap addObject:node];
+    objc_setAssociatedObject(obj, kCWPriorityQueue2ObjKey, priority, OBJC_ASSOCIATION_RETAIN);
+    [self.heap addObject:obj];
 }
 
 -(id)dequeue {
-    CWPriorityQueue2Node *node = [self.heap removeMinimumValue];
-    return node.containedValue;
+    //CWPriorityQueue2Node *node = [self.heap removeMinimumValue];
+    //return node.containedValue;
+    id dequeued = [self.heap removeMinimumValue];
+    objc_setAssociatedObject(dequeued, kCWPriorityQueue2ObjKey, nil, OBJC_ASSOCIATION_RETAIN);
+    return dequeued;
 }
 
 @end
