60ee4013ea2828783c4a5cbca58112f4cb018bd0
Colin Wheeler <cocoasamurai@gmail.com>
2013-11-26 09:05:53 -0600

    document -enqueue:withPriority: and -dequeue

--- ObjC_Playground/CWPriorityQueue2.h
+++ ObjC_Playground/CWPriorityQueue2.h
@@ -10,7 +10,22 @@
 
 @interface CWPriorityQueue2 : NSObject
 
+/**
+ Enqueues the object with a given priority
+ 
+ @param obj the item to the added to the queue
+ @priority a NSNumber representing the priority. 0 is highest priority
+ */
 -(void)enqueue:(id)obj withPriority:(NSNumber *)priority;
+
+/**
+ Removes the item with the highest priority from the queue
+ 
+ If multiple items all have the highest priority then this will return one of
+ the high priority items.
+ 
+ @return an item off the queue with the highest priority or nil
+ */
 -(id)dequeue;
 
 @end
