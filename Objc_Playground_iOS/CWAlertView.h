fe309f1650e00a647d8a6a42fbb30b67e31e12b1
Colin Wheeler <cocoasamurai@gmail.com>
2013-12-13 09:47:58 -0600

    add CWAlertView

---
+++ Objc_Playground_iOS/CWAlertView.h
@@ -0,0 +1,13 @@
+//
+//  CWAlertView.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 12/13/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CWAlertView : UIView
+
+@end
---
+++ Objc_Playground_iOS/CWAlertView.m
@@ -0,0 +1,31 @@
+//
+//  CWAlertView.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 12/13/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWAlertView.h"
+
+@implementation CWAlertView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
