7c52af883bdf4fb5be722f50372fc2f161c26883
Colin Wheeler <cocoasamurai@gmail.com>
2013-12-14 10:37:16 -0600

    CWAlertViewController

---
+++ Objc_Playground_iOS/CWAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  CWAlertViewController.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 12/14/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CWAlertViewController : UIViewController
+
+@end
---
+++ Objc_Playground_iOS/CWAlertViewController.m
@@ -0,0 +1,38 @@
+//
+//  CWAlertViewController.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 12/14/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWAlertViewController.h"
+
+@interface CWAlertViewController ()
+
+@end
+
+@implementation CWAlertViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
