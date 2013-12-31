c1c2b8ac967feef87c596939dfd7fb820c75e0c1
Colin Wheeler <cocoasamurai@gmail.com>
2013-12-31 09:54:39 -0600

    CWTrie2NodeCreate() and CWTrie2NodeEqual()

--- ObjC_Playground/CWTrie2.mm
+++ ObjC_Playground/CWTrie2.mm
@@ -13,6 +13,27 @@ typedef struct CWTrie2Node {
     char *key;
 } CWTrie2Node;
 
+CWTrie2Node *CWTrie2NodeCreate(id *value,char key) {
+    CWTrie2Node *node = calloc(1, sizeof(CWTrie2Node));
+    node->value = value;
+    node->key = ({
+        char *str = calloc(2, sizeof(char));
+        strncpy(str, &key, 1);
+        str;
+    });
+    return node;
+}
+
+BOOL CWTrie2NodeEqual(CWTrie2Node *node1, CWTrie2Node *node2) {
+    BOOL keyEqual = ({
+        BOOL result = YES;
+        if (strcmp(node1->key, node2->key) != 0) result = NO;
+        result;
+    });
+    BOOL objEqual = [(id)node1->value isEqual:(id)node2->value];
+    return (keyEqual && objEqual);
+}
+
 @interface CWTrie2 ()
 @property(assign) CWTrie2Node *root;
 @end
