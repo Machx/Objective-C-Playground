2e2fe6808454682c1f11dae51371e0336c4ea43b
Colin Wheeler <cocoasamurai@gmail.com>
2013-12-31 09:34:48 -0600

    add CWTrie2

---
+++ ObjC_Playground/CWTrie2.h
@@ -0,0 +1,13 @@
+//
+//  CWTrie2.h
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 12/31/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWTrie2 : NSObject
+
+@end
---
+++ ObjC_Playground/CWTrie2.mm
@@ -0,0 +1,13 @@
+//
+//  CWTrie2.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 12/31/13.
+//  Copyright (c) 2013 Colin Wheeler. All rights reserved.
+//
+
+#import "CWTrie2.h"
+
+@implementation CWTrie2
+
+@end
