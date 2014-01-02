5becc49deb97a56cea7e323e0809807e20d9f9cb
Colin Wheeler <cocoasamurai@gmail.com>
2014-01-02 11:12:08 -0600

    setup node

--- ObjC_Playground/CWTrie2.mm
+++ ObjC_Playground/CWTrie2.mm
@@ -8,6 +8,39 @@
 
 #import "CWTrie2.h"
 
+@interface CWTrie2Node : NSObject
+@property(assign) char key;
+@property(strong) id storedValue;
+@property(copy) NSMutableSet *children;
+@end
+
+@implementation CWTrie2Node
+
++(CWTrie2Node *)nodeWithKey:(char)ch
+                   andValue:(id)aVal {
+    CWTrie2Node *node = [self new];
+    node.key = ch;
+    node.storedValue = aVal;
+    return node;
+}
+
+-(instancetype)init {
+    self = [super init];
+    if(!self) return nil;
+    
+    _key = NULL;
+    _storedValue = nil;
+    _children = [NSMutableSet set];
+    
+    return self;
+}
+
+@end
+
+@interface CWTrie2 ()
+//lock or queue...
+@end
+
 @implementation CWTrie2
 
 -(instancetype)init {
