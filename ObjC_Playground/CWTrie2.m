1d3502f8b42aeb6ec596fcc23ad7edebdc0aa5c0
Colin Wheeler <cocoasamurai@gmail.com>
2014-01-03 09:13:10 -0600

    mm -> m

--- ObjC_Playground/CWTrie2.m
+++ ObjC_Playground/CWTrie2.m
@@ -7,6 +7,7 @@
 //
 
 #import "CWTrie2.h"
+#import <libkern/OSAtomic.h>
 
 @interface CWTrie2Node : NSObject
 @property(assign) char key;
@@ -38,8 +39,11 @@
 @end
 
 @interface CWTrie2 ()
-//lock or queue...
+@property(strong) CWTrie2Node *root;
+@property(strong) dispatch_queue_t queue;
 @end
+
+static uint32_t queue_counter = 0;
 
 @implementation CWTrie2
 
@@ -47,9 +51,24 @@
     self = [super init];
     if(!self) return self;
     
+    _root = nil;
+    _queue = ({
+        NSString *classString = NSStringFromClass([self class]);
+        NSString *label = [NSString stringWithFormat:@"%@%i",
+                           classString,OSAtomicIncrement32(&queue_counter)];
+        dispatch_queue_t aQueue = dispatch_queue_create([label UTF8String], 0);
+        aQueue;
+    });
+    
     return self;
 }
 
-
+-(void)setObjectValue:(id)value
+               forKey:(NSString *)key {
+    dispatch_async(self.queue, ^{
+        const char *keyValue = [key UTF8String];
+        
+    });
+}
 
 @end
