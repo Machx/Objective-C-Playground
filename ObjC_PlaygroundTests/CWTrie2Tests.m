4b61d822d5895718e29fb43e21b509b99537f19b
Colin Wheeler <cocoasamurai@gmail.com>
2014-01-05 10:21:29 -0600

    basic unit test for CWTrie2

---
+++ ObjC_PlaygroundTests/CWTrie2Tests.m
@@ -0,0 +1,21 @@
+//
+//  CWTrie2Tests.m
+//  ObjC_Playground
+//
+//  Created by Colin Wheeler on 1/5/14.
+//  Copyright (c) 2014 Colin Wheeler. All rights reserved.
+//
+
+#import "CWTrie2.h"
+
+SpecBegin(CWTrie2)
+
+it(@"should store and retreieve a object", ^{
+    CWTrie2 *trie = [[CWTrie2 alloc] init];
+    
+    [trie setObjectValue:@"World!" forKey:@"Hello"];
+    
+    expect([trie objectValueForKey:@"Hello"]).to.equal(@"World!");
+});
+
+SpecEnd
